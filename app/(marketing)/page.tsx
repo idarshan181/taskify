5b928e9292f3165403d3872a45d38a63c3fbc687
Darshan Patel <work.idarshan18@gmail.com>
2024-08-17 01:22:11 -0230

    feat: marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { Medal } from "lucide-react";
+import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
+import Link from "next/link";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+  display: "swap",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 const MarketingPage = () => {
   return (
-    <div className="flex items-center justify-center flex-col">
-      <div className="flex items-center justify-center flex-col">
+    <div className="flex items-center justify-center flex-col ">
+      <div
+        className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 Task Management App
@@ -16,9 +36,19 @@ const MarketingPage = () => {
           Work forward.
         </div>
       </div>
-      Collaborate, manage projects, and reach new productivity peaks. From high
-      rises to the home office, the way your team works is unique—accomplish it
-      all with Taskify.
+      <div
+        className={cn(
+          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl mx-auto text-center",
+          poppins.className
+        )}
+      >
+        Collaborate, manage projects, and reach new productivity peaks. From
+        high rises to the home office, the way your team works is
+        unique—accomplish it all with Taskify.
+      </div>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/sign-up"> Get Taskify for Free</Link>
+      </Button>
     </div>
   );
 };
